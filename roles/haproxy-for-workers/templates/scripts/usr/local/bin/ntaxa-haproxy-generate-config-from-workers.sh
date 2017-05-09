5dbc5ecca6911601cad48e02072ff6389095968f
Oles Zaburrannyi <kakabomba@gmail.com>
2017-05-09 20:11:34 +0300

    a lot

--- roles/haproxy-for-workers/templates/scripts/usr/local/bin/ntaxa-haproxy-generate-config-from-workers.sh
+++ roles/haproxy-for-workers/templates/scripts/usr/local/bin/ntaxa-haproxy-generate-config-from-workers.sh
@@ -5,50 +5,33 @@ source /usr/local/bin/o-lib.sh
 f=150
 t=200
 ch="a-zA-Z0-9_-"
-#a="\\(\\(*\\.\\)|\\([$ch][-$ch]\\).\\)*[$ch][-$ch]\\{1,50\\}.[$ch]\{1,30\}\$"
 onel="\\(\\(\\([$ch]\\{1,30\\}\\)\\|\\(\\*\\)\\)\\.\\)"
 a="^$onel*\\([$ch]\\{1,30\\}\\.\\)[$ch]\\{1,10\\}\$"
-ddir='aliases'
-ddirweb='aliasesweb'
-rootdir='/usr/local/bin/haproxy'
 newl="
 "
+__deb ()
+{ 
+  echo "$*" 1>&2
+} 
+  
+ippref='10.10.13.'
+hostpref='n.ntaxa.com'
+external_ip=88.99.238.13 
+ippref='10.10.13.'
+rootdir="/usr/local/bin/haproxy/$external_ip"
+newl="
+"   
+  
+ddir="$rootdir"
+#ddirweb="$rootdir/aliasesweb"
+allcertsdir="$rootdir/certs"
 
-rm -r "$rootdir/$ddir/"*
-rm -r "$rootdir/$ddirweb/"*
+rm -r "$ddir/"*
+#rm -r "$ddirweb/"*
+#rm -r "$allcertsdir/"*
 
-ddir="$rootdir/$ddir"
-ddirweb="$rootdir/$ddirweb"
-
-fw ()
-{
-	vma=$1
-        hna=$2
-	ipa=$3
-	alsa=$4
-        echo "alsa=$alsa"
-        usebackendfile="$ddir/$vma/$hna.use_backend.cfg"
-        conditionlines=''
-        conditions=''
-	echo "+	Writing file $aliasfilename for $vma"
-#	echo "    use_backend $vma if { hdr_end(host) -i $hna }"
-#        echo "    use_backend $vma if { hdr_end(host) -i $hna }" > $usebackendfile
-	    for alias in $alsa; do
-	      echo "alias $alias"
-	      if [[ "$alias" =~ [*] ]] ; then
-                aliasconditin='hdr_reg(host) -i ^'$(echo $alias | sed -e 's/\./\\./gi' -e 's/\*/.*/gi')'$'
-              else
-		aliasconditin="hdr(host) -i $alias"
-              fi
-	      if [[ ${#conditions} -gt 150 ]]; then
-                conditionlines="$conditionlines$newl$conditions"
-                conditions=''
-              fi
-              conditions="$conditions or { $aliasconditin }"
-            done
-	    conditionlines="$conditionlines$newl$conditions"
-	echo "$conditionlines" | grep "host" | sed -e "s/^ or /    use_backend $vma if /gi" > $usebackendfile
-  return 0
+backend_name () {
+  echo $hostpref-$1
 }
 
 html ()
@@ -61,9 +44,189 @@ html ()
   return 0
 }
 
-for ip in {0..99}; do
-  echo $ip
+ip_for_domain () {
+  host -ta $1 | grep 'has address' | head -n1 | sed -e 's/.* has address //g'
+}
+
+if_fqdn_and_our_ip () {
+  __deb '->'" if_fqdn_and_our_ip $*"
+  local ret=''
+  local ifip=$1
+  local d=''
+  shift
+  for d in $*; do
+    local d_ip=$(ip_for_domain $d)
+    if is_fqdn $d; then 
+      if [[ "$d_ip" == "$ifip" ]]; then
+        ret="$ret $d"
+      else
+        __deb "domain $d has wrong ip ($d_ip != $ifip) ignored"
+      fi
+    else
+        __deb "domain $d is not fqdn. ignored"
+    fi
+  done
+  __deb '<-'" if_fqdn_and_our_ip $ret"
+  echo $ret
+}
+
+generate_certificate () {
+  __deb '->'" generate_certificate $*"
+  local ssltype=$1
+  local certdir=$2
+  local ifip=$3
+  local proj_name=$4
+  local domains="${@:5}"
+  local d=''
+  if [[ "$ssltype" == 'no' ]]; then
+    echo ''
+  fi
+  if [[ "$ssltype" == 'auto' ]]; then
+    ssl_domains=$(if_fqdn_and_our_ip $ifip $domains)
+    echo "ssl_domains $ssl_domains"
+    if [[ $ssl_domains != '' ]]; then
+      chainkey=$(certonly.sh ntaxa@ntaxa.com $ssl_domains)
+      fullchain=$(echo "$chainkey" | grep 'Certificate Path: ' | sed -e 's/^.*:\s\+//g')
+      privkey=$(echo "$chainkey" | grep 'Private Key Path: ' | sed -e 's/^.*:\s\+//g')
+      if [[ -f $fullchain && -f $privkey ]]; then
+        cat $fullchain $privkey > "$certdir/$project_name.pem"
+      fi
+    fi
+  fi
+  if [[ "$ssltype" == 'yes' ]]; then
+    fullchain_privkey="$(ssh -oBatchMode=yes $ip /usr/local/bin/ntaxa-apache-list-hosts.sh $proj_name)"
+    cat $fullchain_privkey > "$certdir/$project_name.pem"
+  fi
+  __deb '<-'" generate_certificate"
+}
+
+generate_redirections () {
+  local ssltype=$1
+  local external_ip=$2
+  local p_n=$3
+  local domains="${@:4}"
+  local d
+  local tohttp="$ddir/$ip/to_http_redirection.cfg"
+  local tohttps="$ddir/$ip/to_https_redirection.cfg"
+  local conditionlines=''
+  local conditions=''
+  local aliasconditin=''
+  for d in $domains; do
+    if [[ "$d" =~ [*] ]] ; then
+      aliasconditin='hdr_reg(host) -i ^'$(echo $alias | sed -e 's/\./\\./gi' -e 's/\*/.*/gi')'$'
+    else
+      aliasconditin="hdr(host) -i $alias"
+    fi
+	if [[ ${#conditions} -gt 150 ]]; then
+      conditionlines="$conditionlines$newl$conditions"
+      conditions=''
+    fi
+      conditions="$conditions or { $aliasconditin }"
+  done
+  conditionlines=$(strip_new_lines "$conditionlines$newl$conditions")
+  if [[ "$conditionlines" != "" && $conditions != "" ]]; then
+    if [[ "$ssltype" == 'auto' || "$ssltype" == 'yes' ]]; then
+      echo "
+# project $p_n" >> $tohttps
+      echo "$conditionlines" | sed -e "s/^ or /    redirect scheme https code 301 if !{ssl_fc} /gi" >> $tohttps
+    else
+      echo "
+# project $p_n" >> $tohttp
+      echo "$conditionlines" | sed -e "s/^ or /    redirect scheme http code 301 if {ssl_fc} /gi" >> $tohttp
+    fi
+  fi
+} 
+
+generate_use_backend ()
+{
+  ip=$1
+  p_n=$2
+  alsa=$3
+  usebackendfile="$ddir/$ip/use_backend.cfg"
+  conditionlines=''
+  conditions=''
+  for alias in $alsa; do
+    if [[ "$alias" =~ [*] ]] ; then
+      aliasconditin='hdr_reg(host) -i ^'$(echo $alias | sed -e 's/\./\\./gi' -e 's/\*/.*/gi')'$'
+    else
+      aliasconditin="hdr(host) -i $alias"
+    fi
+	if [[ ${#conditions} -gt 50 ]]; then
+      conditionlines="$conditionlines$newl$conditions"
+      conditions=''
+    fi
+      conditions="$conditions or { $aliasconditin }"
+  done
+  conditionlines=$(strip_new_lines "$conditionlines$newl$conditions")
+  if [[ "$conditionlines" != "" ]]; then
+    echo "
+# project $p_n" | tee -a $usebackendfile
+    echo "$conditionlines" | sed -e "s/^ or /    use_backend $(backend_name $ip) if /gi" |  tee -a $usebackendfile
+  fi
+}
+
+
+append_from_internal_ip () {
+   local ip=$1
+   local filename=$2
+   if [[ -f $ddir/$ip/$filename  ]]; then
+     echo "
+#from file $ddir/$ip/$filename" >> $ddir/$filename
+    cat $ddir/$ip/$filename >> $ddir/$filename
+   fi
+}
+
+for ipnum in {3..5}; do
+  ip=$ippref$ipnum
+  __deb checking ip $ip
+  host_is_up $ip
+  if [[ $? == '1' ]]; then
+    mkdir -p $ddir/$ip
+    certdir=$ddir/$ip/certs
+    mkdir -p $certdir
+    mkdir -p $ddirweb/$ip
+    projects="$(ssh -oBatchMode=yes $ip /usr/local/bin/ntaxa-apache-list-hosts.sh)"
+    generate_use_backend $ip host "$ip.$hostpref"
+    if [[ "$projects" != "" ]]; then
+      echo "$projects" | while read -r delvar1 project_name delvar2 ssltype delvar3 domains; do
+        generate_use_backend $ip $project_name "$domains"
+        generate_certificate $ssltype $ddir/$ip/certs $external_ip $project_name "$domains"
+        generate_redirections $ssltype $external_ip $project_name "$domains"
+      done
+    else
+      __deb there is no ssh connection to host or no projects there
+    fi
+    echo "backend $(backend_name $ip)" > $ddir/$ip/backend.cfg
+    echo "  server $hostpref-$ip $ip:80" >> $ddir/$ip/backend.cfg
+  else
+    __deb host is down
+  fi
 done
+
+for ip in $(ls $ddir/); do
+  __deb ip is $ip
+  append_from_internal_ip $ip backend.cfg
+  append_from_internal_ip $ip use_backend.cfg
+  append_from_internal_ip $ip to_http_redirection.cfg
+  append_from_internal_ip $ip to_https_redirection.cfg
+done
+
+cat ./haproxy/haproxy.cfg.template | sed "/----backends----/ {
+  r $ddir/backends.cfg
+  d
+}" | sed "/----use_backends----/ {
+  r $ddir/use_backends.cfg
+  d
+}" | sed "/----to_http_redirections----/ {
+  r $ddir/$ip/to_http_redirections.cfg
+  d
+}" | sed "/----to_https_redirections----/ {
+  r $ddir/$ip/to_https_redirections.cfg
+  d
+}" > /etc/haproxy/haproxy.cfg
+
+service haproxy restart
+
 exit
 for vmpath in /images/private/*; do
   vm=`basename $vmpath`
